1930ac2b4f67c82126420d890750221d7e79f16e
Diego Garcia <diego.garcia2084@alumnos.udg.mx>
2020-10-02 18:00:52 -0500

    prueba 03

--- main.cpp
+++ main.cpp
@@ -3,7 +3,26 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string datoPrimitivo;
+    cin>> datoPrimitivo;
+
+    if(datoPrimitivo == "char")
+        cout<<sizeof(char);
+
+    if(datoPrimitivo == "int")
+        cout<<sizeof(int);
+    
+    if(datoPrimitivo == "short")
+        cout<<sizeof(short);
+    
+    if(datoPrimitivo == "float")
+        cout<<sizeof(float);
+    
+    if(datoPrimitivo == "double")
+        cout<<sizeof(double);
+    
+    if(datoPrimitivo == "long")
+        cout<<sizeof(long);
 
     return 0;
 }
